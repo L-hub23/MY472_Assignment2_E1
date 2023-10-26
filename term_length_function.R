6855627721c59e5272d78977c9008203503dbd22
L-hub23 <leaschiller99@icloud.com>
2023-10-26 21:34:45 +0200

    Fixed typos

--- term_length_function.R
+++ term_length_function.R
@@ -5,7 +5,7 @@ length_terms <- function(data, colours = FALSE) {
   president_terms$term_length <- data$end - data$start
   if (colours == TRUE) {
     # Adding column "party_colors" with Red for Republican and Blue for Democrats
-    president_terms$party_colors <- ifelse(president_terms$party == "Republican", "Red", "Blue")
+    president_terms$party_colours <- ifelse(president_terms$party == "Republican", "Red", "Blue")
   }
   return(president_terms)
 }
