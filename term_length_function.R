ef0029b29d7fb5bcd42b3ea2dd5f49753689bab6
L-hub23 <leaschiller99@icloud.com>
2023-10-26 20:56:47 +0200

    Added comments to function code

--- term_length_function.R
+++ term_length_function.R
@@ -1,10 +1,12 @@
 length_terms <- function(data) {
   president_terms <- data
+  # Adding term_length column showing the time difference 
+  # between the start and end of term:
   president_terms$term_length <- data$end - data$start
   return(president_terms)
 }
 
-# This function can for example be used on the dataset "presidential"
+# This function can for example be used on the data set "presidential"
 # from the ggplot2 package:
 library("ggplot2")
 print(length_terms(presidential))
